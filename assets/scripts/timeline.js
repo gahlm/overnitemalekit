52e6c1e7107e3264985423640681f52944b30a02
gahlm <66396443+gahlm@users.noreply.github.com>
2022-02-13 15:11:52 -0600

    roughed in about us and quarantined contact block into a template twig file to fix it later everywhere. (#30)

--- assets/scripts/timeline.js
+++ assets/scripts/timeline.js
@@ -10,7 +10,7 @@ const wave = document.querySelector(".wave");
 const firstDot = document.querySelector("#dot1");
 const thirdDot = document.querySelector("#dot3");
 // add active and last classes
-window.onload = function () {
+window.onload = function() {
 	if (!!first && !!last) {
 		first.classList.remove("next_slide");
 		first.classList.add("active");
@@ -19,7 +19,7 @@ window.onload = function () {
 	}
 };
 // handle sliding classes along loop
-const startSlider = (type) => {
+const startSlider = type => {
 	const active = document.querySelector(".active");
 	const last_slide = document.querySelector(".last_slide");
 	let next = active.nextElementSibling;
@@ -49,29 +49,25 @@ const startSlider = (type) => {
 // sliding the wave animation along the loop
 function moveRight() {
 	wave.classList.toggle("play");
-	setTimeout(function () {
+	setTimeout(function() {
 		wave.classList.toggle("play");
 	}, 700);
 }
 function moveLeft() {
 	wave.classList.toggle("playBackward");
-	setTimeout(function () {
+	setTimeout(function() {
 		wave.classList.toggle("playBackward");
 	}, 700);
 }
 // chevron function calls
 if (!!nextBtn) {
 	nextBtn.addEventListener("click", () => {
-		// if (window.location.pathname == "/history") {
-		// }
 		startSlider();
 		moveRight();
 	});
 }
 if (!!prevBtn) {
 	prevBtn.addEventListener("click", () => {
-		// if (window.location.pathname == "/history") {
-		// }
 		startSlider("prev");
 		moveLeft();
 	});
@@ -79,18 +75,13 @@ if (!!prevBtn) {
 // dot function calls
 if (!!firstDot) {
 	firstDot.addEventListener("click", () => {
-		// if (window.location.pathname == "/history") {
-		// }
 		startSlider("prev");
 		moveLeft();
 	});
 }
 if (!!thirdDot) {
 	thirdDot.addEventListener("click", () => {
-		// if (window.location.pathname == "/history") {
-		// }
 		startSlider();
 		moveRight();
 	});
 }
-
