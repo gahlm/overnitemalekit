510e9c571b81b7138995a7f3b92bcf9921b27bfe
Ian Gloude <igloude@users.noreply.github.com>
2022-02-21 17:13:25 -0600

    Product Layouts (#35)
    * initial layout loop and fields setup
    
    * nested extend pattern
    
    * filled out specialty fields and layout template

--- template-product.php
+++
@@ -1,9 +0,0 @@
-
-   
-<?php
-/* Template Name: Product Page */
-
-$context = Timber::context();
-$post = new TimberPost();
-$context['post'] = $post;
-Timber::render( 'template-product.twig', $context );
