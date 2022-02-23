f38ac709d25f5c29639a7d4c57c71346daf1f082
gahlm <gary@streamdigital.co>
2022-02-23 17:43:58 -0600

    doing stupid things adding small changes

--- functions.php
+++ functions.php
@@ -163,6 +163,16 @@ function fix_svg() {
         </style>';
 }
 add_action( 'admin_head', 'fix_svg' );
+// relative paths
+function switch_to_relative_url($html, $id, $caption, $title, $align, $url, $size, $alt)
+{
+	$imageurl = wp_get_attachment_image_src($id, $size);
+	$relativeurl = wp_make_link_relative($imageurl[0]);   
+	$html = str_replace($imageurl[0],$relativeurl,$html);
+	      
+	return $html;
+}
+add_filter('image_send_to_editor','switch_to_relative_url',10,8);
 
 // removes default post editor
 function remove_editor() {
