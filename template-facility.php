eae3878c8c195d46c4d636ca43a6188af0b04f75
gahlm <gary@streamdigital.co>
2022-02-28 16:59:03 -0600

    catch up local

--- template-facility.php
+++
@@ -1,9 +0,0 @@
-
-   
-<?php
-/* Template Name: Facility Page */
-
-$context = Timber::context();
-$post = new TimberPost();
-$context['post'] = $post;
-Timber::render( 'template-facility.twig', $context );
