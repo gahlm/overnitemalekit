b7812df49c5542b49b1c56572a7b38dbb05b32f6
gahlm <66396443+gahlm@users.noreply.github.com>
2022-03-02 15:41:11 -0600

    Locations (#45)
    * started adding the individual facility template
    
    * updated functions.php
    
    * doing stupid things adding small changes
    
    * update
    
    * started adding the individual facility template
    
    * catch up local
    
    * rebase
    
    * added facility single-facility.twig

--- functions.php
+++ functions.php
@@ -154,6 +154,12 @@ function cc_mime_types( $mimes ){
 }
 add_filter( 'upload_mimes', 'cc_mime_types' );
 
+function remove_editor() {
+  remove_post_type_support('page', 'editor');
+}
+
+add_action('admin_init', 'remove_editor');
+
 function fix_svg() {
   echo '<style type="text/css">
         .attachment-266x266, .thumbnail img {
@@ -163,20 +169,3 @@ function fix_svg() {
         </style>';
 }
 add_action( 'admin_head', 'fix_svg' );
-// relative paths
-function switch_to_relative_url($html, $id, $caption, $title, $align, $url, $size, $alt)
-{
-	$imageurl = wp_get_attachment_image_src($id, $size);
-	$relativeurl = wp_make_link_relative($imageurl[0]);   
-	$html = str_replace($imageurl[0],$relativeurl,$html);
-	      
-	return $html;
-}
-add_filter('image_send_to_editor','switch_to_relative_url',10,8);
-
-// removes default post editor
-function remove_editor() {
-  remove_post_type_support('page', 'editor');
-}
-
-add_action('admin_init', 'remove_editor');
