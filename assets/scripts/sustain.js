62544a61f7cd5d6cb32af6e9ff78516f1cc4f440
gahlm <gary@streamdigital.co>
2022-04-01 12:15:21 -0500

    catch up

--- assets/scripts/sustain.js
+++ assets/scripts/sustain.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 document.addEventListener("DOMContentLoaded", function() {
 	const sustainNum = document.querySelector(".sustainability__number");
 	let rate = sustainNum.dataset.rate;
@@ -16,22 +15,3 @@ document.addEventListener("DOMContentLoaded", function() {
 	let countup = setInterval(sustainability, 1000);
 	sustainability();
 });
-=======
-document.addEventListener("DOMContentLoaded", function() {
-	const sustainNum = document.querySelector(".sustainability__number");
-	let rate = sustainNum.dataset.rate;
-	let orgDate = new Date(2010, 1, 1, 00, 00, 0);
-	let orgTime = orgDate.getTime();
-	let orgVal = 1613760000;
-
-	async function sustainability() {
-		let today = new Date().getTime();
-		let t = today - orgTime;
-		let diff = t / (1000 * 60);
-		let currentSustain = Math.round(diff * rate);
-		sustainNum.innerHTML = `${currentSustain.toLocaleString("en-US")} lbs.`;
-	}
-	let countup = setInterval(sustainability, 1000);
-	sustainability();
-});
->>>>>>> 7fe1687bd2c88069b28d23176be7928c8e71631d
--- functions.php
+++ functions.php
@@ -174,6 +174,16 @@ function fix_svg() {
 }
 
 add_action( 'admin_head', 'fix_svg' );
+// relative paths
+function switch_to_relative_url($html, $id, $caption, $title, $align, $url, $size, $alt)
+{
+	$imageurl = wp_get_attachment_image_src($id, $size);
+	$relativeurl = wp_make_link_relative($imageurl[0]);   
+	$html = str_replace($imageurl[0],$relativeurl,$html);
+	      
+	return $html;
+}
+add_filter('image_send_to_editor','switch_to_relative_url',10,8);
 
 
 /*=============================================
