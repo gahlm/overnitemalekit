2b364846154108f77fe5c6e694adf7e10c7b7210
gahlm <66396443+gahlm@users.noreply.github.com>
2022-06-06 10:49:51 -0500

    Alt tags (#73)
    * firmness slider
    
    * better firmness positioning for 0 and 100 cases
    
    * typography & spacing updates
    
    * optimize firmness styles
    
    * spacing
    
    * clean up specialty bedding desktop
    
    * specialty mobile
    
    * simple layout mobile:
    
    * my product updates
    
    * small fixes for live site
    
    * change dot colors
    
    * catch up
    
    * catch master up
    
    * FFs requested changes to site
    
    * added spec sheets page
    
    * fixed filters, fixed horizontal scroll, fixed timeline layout
    
    * font fix on facilities
    
    * add alt tags
    
    Co-authored-by: Ian Gloude <igloude@gmail.com>

--- template-two_column.php
+++
@@ -1,8 +0,0 @@
-
-<?php
-/* Template Name: Two Column */
-
-$context = Timber::context();
-$post = new TimberPost();
-$context['post'] = $post;
-Timber::render( 'template-content-two-column.twig', $context );
